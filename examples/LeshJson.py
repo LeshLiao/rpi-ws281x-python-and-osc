a4b0cc160bfc8fc0caa49d76d4bc75299999a1a7
LeshLiao <dlnx38@gmail.com>
2019-03-12 21:28:43 +0800

    First version ws281x work with osc protocol in RPI

---
+++ examples/LeshJson.py
@@ -0,0 +1,54 @@
+import json
+import socket
+
+def ReadJsonFile():
+    input_file = open ('config/test01.json')
+    json_Data = json.load(input_file)
+    print("ProjectName:" +json_Data['ProjectName'])
+    json_array = json_Data['MyStations']
+    #global CurrentRuleList
+    CurrentRuleList = []
+    
+    for item in json_array:
+        print("IP:" + item['IP'])
+        print("Port:" + str(item['Port']))
+        if(GetLocalIp() == item['IP']):
+            CurrentRuleList = item['Rules']
+            print("We found RuleList():"+GetLocalIp())
+            break
+    
+    InitDataWs(CurrentRuleList)
+    InitDataDmx(CurrentRuleList)
+
+    
+DataWs = []
+def InitDataWs(_ruleList): 
+    global DataWs    
+    index = 0
+    for _rule in _ruleList:
+        if(_rule['OutputType'] == "WS281X"):
+            DataWs.append([])
+            DataWs[-1].append(index)
+            for _param in _rule['OutputParam']:
+                DataWs[-1].append(_param)
+            
+            #print(str(index)+"_rule:"+_rule['OutputType']) 
+            index += 1
+            
+    print(DataWs)        
+        
+
+def InitDataDmx(_ruleList): 
+    DataDmx = [] 
+    
+    
+     
+def GetLocalIp(): 
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    MyLocalIp = s.getsockname()[0]
+    #print(MyLocalIp)
+    s.close()
+    return MyLocalIp
+
+    
---
+++ examples/osc_server.py
@@ -0,0 +1,113 @@
+"""Small example OSC server
+
+This program listens to several addresses, and prints some information about
+received packets.
+"""
+import argparse
+import math
+
+import LeshLib
+import threading
+
+from pythonosc import dispatcher
+from pythonosc import osc_server
+
+from strandtest import *
+from LeshJson import *
+
+
+def LightWS(_dataList):
+    global DataWs  
+    for _data in DataWs:
+        index = _data[0]
+        colorR, colorG, colorB = LeshLib.GetColorByVolume(int(_dataList[index]))
+        for j in range(_data[1],_data[2]):
+            #print(str(j)+',')
+            strip.setPixelColor(j,Color(colorR,colorG,colorB))
+        #print('|')
+    strip.show()
+
+def LightDMX(_dataList):
+    global DataWs  
+
+def handler_Instruction(unused_addr, args,DelayTime):
+    """
+    print("Instruction Start")
+    print("Instruction End")
+    """
+    global TestNum
+    TestNum = 255
+
+   
+def job():
+  #for i in range(5):
+  global TestNum
+  while True:
+    if(TestNum > 0):
+        for j in range(8):
+            strip.setPixelColor(j,Color(TestNum,TestNum,TestNum))
+        #print("Child thread:", TestNum)
+        strip.show()
+        TestNum = TestNum - 4
+    time.sleep(0.01)
+
+def handler_MatrixVelocity(unused_addr, args,MarixString):
+    tempList = MarixString.split(',')
+
+    if (True):
+        LightWS(tempList)
+        
+    if (True):
+        LightDMX(tempList)
+
+    """
+    tempList = MarixString.split(',')
+    ListIndex = 0
+    maxlen = len(tempList)-1
+    for i in range(0,maxlen):
+        tempInt = int(tempList[i])
+        colorR, colorG, colorB = LeshLib.GetColorByVolume(tempInt)
+        strip.setPixelColor(i,Color(colorR,colorG,colorB))
+    strip.show()
+    #$print("print_handler_TagAndVelocity:[{0}]:{1},{2}".format(args[0],args[1],args[2]))
+    """
+
+
+def print_compute_handler(unused_addr, args, volume):
+  try:
+    print("[{0}] ~ {1}".format(args[0], args[1](volume)))
+  except ValueError: pass
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--ip",
+      default="172.20.10.2", help="The ip to listen on.")
+  parser.add_argument("--port",
+      type=int, default=2346, help="The port to listen on.")
+  args = parser.parse_args()
+
+  #10.1.1.6
+  
+  dispatcher = dispatcher.Dispatcher()
+  dispatcher.map("/MatrixVelocity", handler_MatrixVelocity,"PrintValueAAA")
+  dispatcher.map("/Instruction", handler_Instruction,"test")
+  
+  #initled()
+  #opt_parse()
+  
+  strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+  strip.begin()
+  colorWipe(strip, Color(255, 255, 255))  # White wipe
+  
+  TestNum = 255
+  t = threading.Thread(target = job)
+  t.setDaemon(True)
+  t.start()
+  
+  ReadJsonFile()
+  
+  server = osc_server.ThreadingOSCUDPServer((args.ip, args.port), dispatcher)
+  print("Serving on(test01) {}".format(server.server_address))
+  server.serve_forever()
+  
+  
