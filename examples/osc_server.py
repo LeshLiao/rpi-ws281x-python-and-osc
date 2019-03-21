e073c9746c25dc71ffa6ba21209b4f118d2c5da6
pi <you@example.com>
2019-03-21 04:45:18 +0000

    test13

--- examples/osc_server.py
+++ examples/osc_server.py
@@ -2,7 +2,7 @@
 
 This program listens to several addresses, and prints some information about
 received packets.
-test pull latest version.1111 test
+test pull latest version.1313 test
 """
 import argparse
 import math
--- git_pull_script.sh
+++ git_pull_script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-cd /home/pi/rpi-ws281x-python-and-osc
+cd rpi-ws281x-python-and-osc
+git fetch --all
 git reset --hard origin/master
 git pull
