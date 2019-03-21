5331610c0c12f9c6a442c7d611c9de5c1ab458c0
pi <you@example.com>
2019-03-21 03:50:09 +0000

    git test88

--- examples/osc_server.py
+++ examples/osc_server.py
@@ -2,7 +2,7 @@
 
 This program listens to several addresses, and prints some information about
 received packets.
-test pull latest version.066
+test pull latest version.888 test
 """
 import argparse
 import math
